39fbbbeae4d855122938afaeafc3ef825a64282c
Brian Kabiro <briankigondu@gmail.com>
2017-06-27 17:37:43 +0300

    fifth

--- five.py
+++ five.py
@@ -1,10 +1,19 @@
 ##get smallest natural number divisible by 1..20
+##lcm(a,b,c) = lcm(a,lcm(b,c))
 def checkIfDivisible():
 	for i in range(2,21):
-		for x in range(2,(i-1)):
-			if (i % x) == 0:
+		for x in range(2,i):
+			if(i % x) == 0:
 				break
 			else:
-				print("this be a prime", i)
+				print("this is a prime",i)
+
 
 checkIfDivisible()
+def gcd(a,b):
+	while b:
+		a,b = b, a % b
+	return a
+
+def lcm(a,b):
+	return a * b // gcd(a,b)
